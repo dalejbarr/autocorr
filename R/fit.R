c2edc2f565a8cc099ce5fed4b68310e85972f535
Dale Barr <dalejbarr3@gmail.com>
2018-06-12 11:42:23 +0100

    first commit

---
+++ R/fit.R
@@ -0,0 +1,75 @@
+#' Fit model via Julia MixedModels package
+#'
+#' Fits a linear mixed-effects model via the Julia MixedModels
+#' package.
+#' 
+#' @param mform Model formula.
+#' @param dat Data frame.
+#' @param jsess Julia session (established by \code{JuliaCall::julia_setup()}).
+#' @param name Name of the Julia object to create.
+#' @return A Julia fitted model object.
+fit_jl <- function(formula, data, jsess, name = "fm1") {
+  jsess$assign("form", formula)
+  jsess$assign("dat", data)
+
+  jsess$eval(paste0(name, " = fit(LinearMixedModel, form, dat)"))
+  jsess$eval(name)
+}
+
+#' Simulate and fit 2x2 autocorrelated data
+#'
+#' Simulate data from a 2x2 mixed design (A within and B between) with
+#' autocorrelated residuals.
+#'
+#' @param n_subj Number of subjects.
+#' @param n_obs Number of observations per subject.
+#' @param fixed vector of fixed effects in the following order:
+#'   Intercept, main effect of A, main effect of B, and AB
+#'   interaction.
+#' @param jsess Julia session.
+#' @param amx Matrix of autocorrelation functions.
+#' @param amx_wt Weights for sampling rows from \code{amx}.
+#' @details Simulate data corresponding to three models: no
+#'   autocorrelation (\code{mod_no}), random trial order
+#'   (\code{mod_rand}), and with trials blocked by levels of A
+#'   (\code{mod_block}). Models are fit via Julia (using
+#'   \code{\link{run_jl}}).
+#' @return A two dimensional array with fixed effects estimates and
+#'   standard errors.
+#' @seealso \code{\link{sim_2x2}}, \code{\link{fit_jl}}
+#' @export
+run_2x2 <- function(n_subj, n_obs, fixed, jsess, amx, amx_wt = NULL) {
+  ## n_subj <- 48L; n_obs <- 48L; fixed <- rep(0, 4); jsess <- j
+  ## rm(n_subj, n_obs, fixed, jsess)
+  ## two-by-two design, everything between
+  my_design <- list(ivs = c(A = 2, B = 2),
+		    n_item = n_obs * 2L,
+		    between_item = c("A", "B"),
+		    between_subj = c("B"))
+
+  parms <- funfact::gen_pop(my_design, n_subj)
+  parms$fixed[] <- fixed
+  parms$err_var <- 6
+
+  dat <- sim_2x2(n_subj, n_obs, parms, amx) %>%
+    funfact::with_dev_pred(c("A", "B"))
+
+  ## fit the models using autocorr::fit_jl
+  mod_no <- fit_jl(Y_acn ~ AA2 * BB2 + (AA2 | subj_id), dat,
+                   jsess, "mod_no")
+  mod_rand <- fit_jl(Y_acr ~ AA2 * BB2 + (AA2 | subj_id), dat,
+                     jsess, "mod_rand")
+  mod_block <- fit_jl(Y_acb ~ AA2 * BB2 + (AA2 | subj_id), dat,
+                      jsess, "mod_block")
+
+  array(c(jsess$eval("StatsBase.coef(mod_no)"),
+          jsess$eval("StatsBase.stderror(mod_no)"),
+          jsess$eval("StatsBase.coef(mod_rand)"),
+          jsess$eval("StatsBase.stderror(mod_rand)"),
+          jsess$eval("StatsBase.coef(mod_block)"),
+          jsess$eval("StatsBase.stderror(mod_block)")),
+        dim = c(4, 2, 3),
+        dimnames = list(coef = c("(Intercept)", "A", "B", "A:B"),
+                        param = c("est", "stderr"),
+                        model = c("mod_no", "mod_rand", "mod_block")))
+}
---
+++ R/sim.R
@@ -0,0 +1,87 @@
+#' @importFrom magrittr "%>%"
+
+#' Simulate autocorrelated residual error based on source function
+#'
+#' Simulate an time series of autocorrelated residual error based on a
+#' source autocorrelation function.
+#' 
+#' @param source_acf Source autocorrelation function to use.
+#' @param length.out Length of the output time series.
+#' @param sigma Desired standard deviation of output time series.
+#' @param center Mean center the time series.
+#' @return A vector of residuals modeled after \code{source_acf}.
+#'
+#' @export
+sim_acerr <- function(source_acf, length.out = 48L, sd = 1, 
+                      center = TRUE) {
+  num_nas <- length(source_acf) - 1L
+  nas_head <- floor(num_nas / 2) + 1L
+  vec <- stats::filter(rnorm(length.out + num_nas, sd = sd), rev(source_acf))
+  res <- vec[seq(nas_head, by = 1L, length.out = length.out)]
+  if (center)
+    res - mean(res)
+  else
+    res
+}
+
+#' Simulate data with autocorrelated errors
+#'
+#' @param n_subj Number of subjects to simulate.
+#' @param n_obs Number of observations per subject.
+#' @param params Population parameter values for the data generating
+#'   process; the structure should be identical to that returned by
+#'   \code{\link{funfact::gen_pop}}.
+#' @param amx Matrix of autocorrelation functions to sample from,
+#'   which each row a separate function.
+#' @param amx_wt Sampling weights for autocorrelation matrix
+#'   (representing the probability of being sampled); \code{NULL}
+#'   gives each row an equal probability.
+#' @details Simulates data from a 2x2 mixed design, with factor A
+#'   within subjects and factor B between subjects.
+#' @return A data frame, with \code{ts_r} as the trial number ordered
+#'   randomly; \code{ts_b} as the trial number blocked by the within
+#'   factor ("A"); \code{Y_fit} as the 'fitted' value (combining fixed
+#'   and random effects but not residual error); \code{Y_acn} as the
+#'   response variable without autocorrelation; \code{Y_acr} as the
+#'   response variable with autocorrelation; and \code{Y_acb} as the
+#'   response variable with autocorrelation for the blocked data.
+#' @export
+sim_2x2 <- function(n_subj, n_obs, params,
+                    amx, amx_wt = NULL) {
+  design_args <- list(ivs = c(A = 2, B = 2),
+                      n_item = n_obs * 2L,
+                      between_item = c("A", "B"),
+                      between_subj = c("B"))
+  dat <- funfact::sim_norm(design_args, n_subj, params, verbose = TRUE) %>%
+    dplyr::mutate(Y_fit = Y - err)
+
+  n_per <- dat %>% dplyr::count(subj_id) %>% dplyr::pull(n) %>% unique()
+  stopifnot(length(n_per) == 1L)
+
+  acerr_all <- purrr::map(sample(seq_len(nrow(amx)), n_subj, TRUE,
+                                 prob = amx_wt),
+                          ~ sim_acerr(amx[.x, ], n_per, params$err_var)) 
+
+  acerr_no_ac <- purrr::map(acerr_all, sample)
+
+  ## trials in random order
+  dat2 <- dat %>%
+    dplyr::group_by(subj_id) %>%
+    dplyr::mutate(ts_r = sample(seq_len(n_per))) %>%
+    dplyr::ungroup() %>%
+    dplyr::arrange(subj_id, ts_r) %>%
+    dplyr::mutate(Y_acr = Y_fit + unlist(acerr_all),
+                  Y_acn = Y_fit + unlist(acerr_no_ac))
+
+  ## trials blocked by level of A (randomized within)
+  dat2 %>%
+    dplyr::group_by(subj_id, A) %>%
+    dplyr::mutate(ts_b = sample(seq_len(n_per / 2) +
+                                (A == "A2") * (n_per / 2))) %>%
+    dplyr::ungroup() %>%
+    dplyr::arrange(subj_id, ts_b) %>%
+    dplyr::mutate(Y_acb = Y_fit + unlist(acerr_all)) %>%
+    dplyr::select(subj_id, item_id, ts_r, ts_b, A, B,
+                  Y_fit, Y_acn, Y_acr, Y_acb)
+}
+
