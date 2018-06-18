e573690568b40881f94b52624a58281b6d90d014
Dale Barr <dalejbarr3@gmail.com>
2018-06-18 17:54:50 +0100

    dw_plot()

--- R/plot.R
+++ R/plot.R
@@ -16,5 +16,27 @@ acf_plot <- function(data, subject, residuals, lag.max = NULL) {
   ggplot2::ggplot(acorr, ggplot2::aes(lag)) +
     ggplot2::geom_ribbon(ggplot2::aes(ymin = lower, ymax = upper), acorr_ci,
                          fill = "red", alpha = .2) +
-      ggplot2::geom_line(ggplot2::aes(y = r, group = subj), alpha = .2)
+      ggplot2::geom_line(ggplot2::aes_(y = ~r, group = subj),
+                         alpha = .2)
+}
+
+#' Plot Durbin-Watson statistics
+#'
+#' Calculate Durbin-Watson statistics for original (and permuted) residuals in the dataset and produce a density plot.
+#'
+#' @inheritParams acf_subj
+#' @importFrom rlang "!!"
+#' @importFrom magrittr "%>%"
+#' @export
+dw_plot <- function(data, subject, residuals, lag.max = NULL) {
+  subj <- rlang::enquo(subject)
+  resid <- rlang::enquo(residuals)
+  dw_o <- dw_subj(data, !!subj, !!resid)
+  dw_p <- dw_subj(permute_resids(data, !!subj, !!resid), !!subj, !!resid)
+
+  dplyr::bind_rows(dw_o %>% dplyr::mutate(residual = "original"),
+                   dw_p %>% dplyr::mutate(residual = "permuted")) %>%
+    ggplot2::ggplot(ggplot2::aes(dw, fill = residual)) +
+      ggplot2::geom_density(alpha = .2) +
+      ggplot2::coord_cartesian(xlim = c(0, 4))
 }
