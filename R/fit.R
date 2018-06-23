e5295783225804bde363c525746f157933778740
Dale Barr <dalejbarr@pm.me>
2018-06-24 00:25:22 +0100

    gamm_2x2

--- R/fit.R
+++ R/fit.R
@@ -41,21 +41,7 @@ fit_jl <- function(formula, data, jsess, name = "fm1") {
 #' @importFrom magrittr %>%
 #' @export
 run_2x2 <- function(n_subj, n_obs, fixed, jsess, amx, amx_wt = NULL) {
-  ## n_subj <- 48L; n_obs <- 48L; fixed <- rep(0, 4); jsess <- j
-  ## rm(n_subj, n_obs, fixed, jsess)
-  ## two-by-two design, everything between
-  my_design <- list(ivs = c(A = 2, B = 2),
-		    n_item = n_obs * 2L,
-		    between_item = c("A", "B"),
-		    between_subj = c("B"))
-
-  parms <- funfact::gen_pop(my_design, n_subj)
-  parms$fixed[] <- fixed
-  parms$item_rfx[,] <- 0
-  parms$err_var <- 6
-
-  dat <- sim_2x2(n_subj, n_obs, parms, amx) %>%
-    funfact::with_dev_pred(c("A", "B"))
+  dat <- gen_2x2(n_subj, n_obs, fixed, amx, amx_wt)
 
   ## fit the models using autocorr::fit_jl
   mod_no <- fit_jl(Y_acn ~ AA2 * BB2 + (AA2 | subj_id), dat,
@@ -75,4 +61,71 @@ run_2x2 <- function(n_subj, n_obs, fixed, jsess, amx, amx_wt = NULL) {
         dimnames = list(coef = c("(Intercept)", "A", "B", "A:B"),
                         param = c("est", "stderr"),
                         model = c("mod_no", "mod_rand", "mod_block")))
+}
+
+#' Simulate and Fit GAMM
+#'
+#' Simulate 2x2 data and fit GAMM models.
+#'
+#' @param n_subj Number of subjects.
+#' @param n_obs Number of observations per subject.
+#' @param fixed vector of fixed effects in the following order:
+#'   Intercept, main effect of A, main effect of B, and AB
+#'   interaction.
+#' @param amx Matrix of autocorrelation functions.
+#' @param amx_wt Weights for sampling rows from \code{amx}.
+#' @importFrom mgcv s
+#' @export
+gamm_2x2 <- function(n_subj, n_obs, fixed, amx, amx_wt = NULL) {
+  dat <- gen_2x2(n_subj, n_obs, fixed, amx, amx_wt) %>%
+    dplyr::mutate(subj_id = factor(subj_id),
+                  tnum_r = (ts_r - (n_obs + 1) / 2) / (n_obs - 1),
+                  tnum_b = (ts_b - (n_obs + 1) / 2) / (n_obs - 1))
+
+  ## fit the models using autocorr::fit_jl
+  mod_no <- mgcv::bam(Y_acn ~ AA2 * BB2 +
+                        s(tnum_r, by = subj_id) +
+                        s(subj_id, bs = "re") +
+                        s(subj_id, AA2, bs = "re"),
+                      data = dat)
+
+  mod_rand <- mgcv::bam(Y_acr ~ AA2 * BB2 +
+                          s(tnum_r, by = subj_id) +
+                          s(subj_id, bs = "re") +
+                          s(subj_id, AA2, bs = "re"),
+                        data = dat)
+
+  mod_block <- mgcv::bam(Y_acb ~ AA2 * BB2 +
+                           s(tnum_b, by = subj_id) +
+                           s(subj_id, bs = "re") +
+                           s(subj_id, AA2, bs = "re"),
+                         data = dat)
+  
+  array(c(coef(mod_no)[1:4],
+          sqrt(diag(vcov(mod_no)[1:4, 1:4])),
+          coef(mod_rand)[1:4],
+          sqrt(diag(vcov(mod_rand)[1:4, 1:4])),
+          coef(mod_block)[1:4],
+          sqrt(diag(vcov(mod_block)[1:4, 1:4]))),
+        dim = c(4, 2, 3),
+        dimnames = list(coef = c("(Intercept)", "A", "B", "A:B"),
+                        param = c("est", "stderr"),
+                        model = c("mod_no", "mod_rand", "mod_block")))
+}
+
+#' @importFrom magrittr "%>%"
+#' @export
+gen_2x2 <- function(n_subj, n_obs, fixed, amx, amx_wt = NULL) {
+  my_design <- list(ivs = c(A = 2, B = 2),
+		    n_item = n_obs * 2L,
+		    between_item = c("A", "B"),
+		    between_subj = c("B"))
+  
+  parms <- funfact::gen_pop(my_design, n_subj)
+  parms$fixed[] <- fixed
+  parms$item_rfx[,] <- 0
+  parms$err_var <- 6
+
+  sim_2x2(n_subj, n_obs, parms, amx, amx_wt) %>%
+    funfact::with_dev_pred(c("A", "B"))
 }
